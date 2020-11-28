ec9ab0fcee13355fa9be335069717c37fb697756
habrador <erik.nordeus@gmail.com>
2020-11-28 12:09:58 +0100

    Realized that decals is just a matter of cutting meshes to fit the decal, so added a separate folder because this project will include other cut algorithms

--- Assets/Test scenes/8. Cut mesh/Decals/DecalsController.cs
+++ Assets/Test scenes/8. Cut mesh/Decals/DecalsController.cs
@@ -55,10 +55,26 @@ public class DecalsController : MonoBehaviour
             //The forward of a built-in quad is inverted
             decalTrans.forward = -hit.normal;
 
-            Debug.Log(hit.point);
+            //Debug.Log(hit.point);
 
             //The object that was hit
-            Transform objectHit = hit.transform;
+            //Transform objectHit = hit.transform;
+
+            DeformDecal(decalTrans);
         }
+    }
+
+
+    //Based on:
+    // - Game Programming Gems 2 (p. 395): Applying decals to arbitrary surfaces 
+    private void DeformDecal(Transform decalTrans)
+    {
+        Vector3 P = decalTrans.position;
+        //If we are using the built-in quad, the orientation becomes:
+        Vector3 N = -decalTrans.forward;
+        Vector3 T = decalTrans.right;
+        Vector3 B = decalTrans.up;
+
+
     }
 }
