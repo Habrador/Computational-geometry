b38514c675248b969aaf13eb5e4d8667eab4f1e5
habrador <erik.nordeus@gmail.com>
2020-11-28 10:36:44 +0100

    Added basic scene where we can place a quad in a scene by raycasting

---
+++ Assets/Test scenes/2. Generate mesh/Decals/DecalsController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecalsController : MonoBehaviour
+{
+    public Transform decalTrans;
+
+    public Camera thisCamera;
+
+
+
+    void Start()
+	{
+        
+	}
+
+    
+
+    void Update()
+    {
+        PlaceDecal();
+    }
+
+
+
+    //Fire a ray from the mouse pos to place the decals in the scene  
+    private void PlaceDecal()
+    {
+        if (thisCamera == null)
+        {
+            Debug.Log("You need a camera");
+
+            return;
+        }
+        if (decalTrans == null)
+        {
+            Debug.Log("You need a decal");
+
+            return;
+        }
+
+
+        RaycastHit hit;
+        
+        Ray ray = thisCamera.ScreenPointToRay(Input.mousePosition);
+
+        //Make sure the decal doesn't have a collider or the ray will hit it!
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            //Place the decal at the position where we hit
+            decalTrans.position = hit.point;
+
+            //The forward of a built-in quad is inverted
+            decalTrans.forward = -hit.normal;
+
+            Debug.Log(hit.point);
+
+            //The object that was hit
+            Transform objectHit = hit.transform;
+        }
+    }
+}
