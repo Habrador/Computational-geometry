94a2884ea427f4f521559f3e12e8287794aba2ea
habrador <erik.nordeus@gmail.com>
2020-12-18 12:48:44 +0100

    Refactored code

--- Assets/_Habrador Computational Geometry Library/8. Deform mesh/Simplification/RemoveUnwantedTriangles.cs
+++ Assets/_Habrador Computational Geometry Library/8. Deform mesh/Simplification/RemoveUnwantedTriangles.cs
@@ -14,7 +14,7 @@ namespace Habrador_Computational_Geometry
         // - One angle in the triangle is close to 0
         // - The ratio between the shortest and longest side is below 0.01
         private const float NEEDLE_RATIO = 0.01f;
-
+        private const float FLAT_TETRAHEDRON_DISTANCE = 0.001f;
 
 
         //meshData should be triangles only
@@ -25,7 +25,10 @@ namespace Habrador_Computational_Geometry
             // - Caps. Triangle where one angle is close to 180 degrees. Are difficult to remove. If the vertex is connected to three triangles, we can maybe just remove the vertex and build one big triangle. This can be said to be a flat terahedron?
 
             // - Needles. Triangle where the longest edge is much longer than the shortest one.  Same as saying that the smallest angle is close to 0 degrees? Can often be removed by collapsing the shortest edge
-            RemoveNeedles(meshData, normalizer);
+            //RemoveNeedles(meshData, normalizer);
+
+            // - Flat tetrahedrons. Find a vertex and if this vertex is surrounded by three triangles, and if all the vertex is roughly on the same plane as one of big triangle, then remove the vertex and replace it with one big triangle
+            RemoveFlatTetrahedrons(meshData, normalizer);
 
             //TODO: The above should be in the same loop because when we have removed a needle we might get a new cap, etc
         }
@@ -111,6 +114,98 @@ namespace Habrador_Computational_Geometry
             while (foundNeedle);
 
             Debug.Log($"Found {needleCounter} needles");
+        }
+
+
+
+        //Remove flat tetrahedrons (a vertex in a triangle)
+        private static void RemoveFlatTetrahedrons(HalfEdgeData3 meshData, Normalizer3 normalizer = null)
+        {
+            HashSet<HalfEdgeVertex3> vertices = meshData.verts;
+
+            int flatTetrahedronCounter = 0;
+
+            bool foundFlatTetrahedron = false;
+
+            int safety = 0;
+
+            do
+            {
+                foundFlatTetrahedron = false;
+
+                foreach (HalfEdgeVertex3 vertex in vertices)
+                {
+                    HashSet<HalfEdge3> edgesGoingToVertex = vertex.GetEdgesGoingToVertex();
+
+                    if (edgesGoingToVertex != null && edgesGoingToVertex.Count == 3)
+                    {
+                        //Find the vertices of the triangle covering this vertex clock-wise
+                        HalfEdgeVertex3 v1 = vertex.edge.v;
+                        HalfEdgeVertex3 v2 = vertex.edge.prevEdge.oppositeEdge.v;
+                        HalfEdgeVertex3 v3 = vertex.edge.oppositeEdge.nextEdge.v;
+
+                        //Build a plane
+                        MyVector3 normal = MyVector3.Normalize(MyVector3.Cross(v3.position - v2.position, v1.position - v2.position));
+
+                        Plane3 plane = new Plane3(v1.position, normal);
+
+                        //Find the distance from the vertex to the plane
+                        float distance = _Geometry.GetSignedDistanceFromPointToPlane(vertex.position, plane);
+
+                        distance = Mathf.Abs(distance);
+
+                        if (distance < FLAT_TETRAHEDRON_DISTANCE)
+                        {
+                            //Debug.Log("Found flat tetrahedron");
+
+                            Vector3 p1 = normalizer.UnNormalize(v1.position).ToVector3();
+                            Vector3 p2 = normalizer.UnNormalize(v2.position).ToVector3();
+                            Vector3 p3 = normalizer.UnNormalize(v3.position).ToVector3();
+
+                            TestAlgorithmsHelpMethods.DebugDrawTriangle(p1, p2, p3, normal.ToVector3(), Color.blue, Color.red);
+
+                            foundFlatTetrahedron = true;
+
+                            flatTetrahedronCounter += 1;
+
+                            //Save the opposite edges
+                            HashSet<HalfEdge3> oppositeEdges = new HashSet<HalfEdge3>();
+
+                            oppositeEdges.Add(v1.edge.oppositeEdge);
+                            oppositeEdges.Add(v2.edge.oppositeEdge);
+                            oppositeEdges.Add(v3.edge.oppositeEdge);
+
+                            //Remove the three triangles
+                            foreach (HalfEdge3 e in edgesGoingToVertex)
+                            {
+                                meshData.DeleteTriangleFace(e.face);
+                            }
+
+                            //Add the new triangle (could maybe connect it ourselves)
+                            HalfEdgeFace3 newTriangle = meshData.AddTriangle(v1.position, v2.position, v3.position, findOppositeEdge: false);
+
+                            meshData.TryFindOppositeEdge(newTriangle.edge, oppositeEdges);
+                            meshData.TryFindOppositeEdge(newTriangle.edge.nextEdge, oppositeEdges);
+                            meshData.TryFindOppositeEdge(newTriangle.edge.nextEdge.nextEdge, oppositeEdges);
+
+                            break;
+                        }
+                    }
+                }
+
+
+                safety += 1;
+
+                if (safety > 100000)
+                {
+                    Debug.LogWarning("Stuck in infinite loop while removing flat terahedrons");
+
+                    break;
+                }
+            }
+            while (foundFlatTetrahedron);
+
+            Debug.Log($"Found {flatTetrahedronCounter} flat tetrahedrons");
         }
     }
 }
